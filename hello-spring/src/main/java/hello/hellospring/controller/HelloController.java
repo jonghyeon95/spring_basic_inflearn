35da171cb9d815db996dd9b6b651e01d214d2841
jonghyeon95 <whdgus5289@naver.com>
2021-07-06 15:27:20 +0900

    api @ResponseBody 통해 HttpMessageConverter를 이용하여 바로 반환 (객체json or string)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,6 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //바디부분에 내용을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //View가 없이 이 내용이 그대로 출력됨
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   //객체타입 Json 형태로 출력
+    }
+
+    static class Hello{
+        private String name;
+        //alt + insert 통해 자동생성
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
 
