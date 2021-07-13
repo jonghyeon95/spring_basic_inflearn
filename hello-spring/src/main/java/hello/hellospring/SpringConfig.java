18167bbd8a959a52a284a610dd77278efac5276d
jonghyeon95 <whdgus5289@naver.com>
2021-07-13 19:14:49 +0900

    자바코드로 스프링빈 등록

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//직접 등록하는법
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
