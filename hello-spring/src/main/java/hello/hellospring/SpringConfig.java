d23c3009be7710ba5d529220564fd38af820f8e7
jonghyeon95 <whdgus5289@naver.com>
2021-08-15 20:39:25 +0900

    AOP 사용 (메서드 끝날때마다 특정함수 실행)

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceApp;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,11 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceApp timeTraceApp(){
+//        return new TimeTraceApp();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository(){
