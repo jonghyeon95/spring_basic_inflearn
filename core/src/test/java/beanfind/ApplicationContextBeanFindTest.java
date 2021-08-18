56206e0fac5954d0ac01be8e08f28bc873f5cb73
jonghyeon95 <whdgus5289@naver.com>
2021-08-18 16:42:50 +0900

    상속관계에 있는 스프링빈 조회, Test코드 작성

--- core/src/test/java/beanfind/ApplicationContextBeanFindTest.java
+++ core/src/test/java/beanfind/ApplicationContextBeanFindTest.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ApplictionContextBeanFindTest {
+public class ApplicationContextBeanFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
