073e51a180f23c8964d4ead5dae4e9c7df37ccc1
jonghyeon95 <whdgus5289@naver.com>
2021-08-18 17:31:45 +0900

    xml기반의 ApplicationContext 생성

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,19 +1,26 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
 
 //    MemberService memberService = new MemberServiceImpl();
     MemberService memberService;
+    ApplicationContext ac;
 
     @BeforeEach
     public void beforeEach(){
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
+
+        ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        memberService = ac.getBean("memberService",MemberService.class);
     }
 
     @Test
