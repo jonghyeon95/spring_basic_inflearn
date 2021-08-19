317af48609676733be87f354dfe754cb6f900e9f
jonghyeon95 <whdgus5289@naver.com>
2021-08-19 15:24:04 +0900

    CompnentScan의 필터기능

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)   //Configuration 제외하고 스캔하겠다
+//        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //Configuration 제외하고 스캔하겠다
+)
 public class AutoAppConfig {
 
 
