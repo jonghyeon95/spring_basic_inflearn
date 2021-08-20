297755c46d5b795b5e73c6e5622cb0fae79f10dc
jonghyeon95 <whdgus5289@naver.com>
2021-08-20 17:52:29 +0900

    @Bean (initMethod=" ", destroyMethod=" ") 를 통한 빈의 생명주기 확인

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,15 +30,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { //의존관계셋팅 후 실행
+    public void init() { //의존관계셋팅 후 실행
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {    //종료시 호출
+    public void close() {    //종료시 호출
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
