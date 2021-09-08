ee5eb93ff3cf91066064c78681de20d426b7e8bb
jonghyeon95 <whdgus5289@naver.com>
2021-09-08 17:47:23 +0900

    타임리프 기본기능 -연산

--- spring_MVC_2/thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
+++ spring_MVC_2/thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
@@ -74,6 +74,12 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "spring!");
+        return "basic/operation";
+    }
 
 
     @Component("helloBean")
