f79c401dee8463baa9300f4553a3088a6ac38f3e
jonghyeon95 <whdgus5289@naver.com>
2021-09-11 20:34:45 +0900

    @SessionAttribute 를 통해 더 쉽게 Session정보를 가져올 수 있음.
    첫 로그인시 URL에 쿠키정보가 뜨게 됨(쿠기 미지원 페이지를 위해)
    server.servlet.session.tracking-modes=cookie 로 제거

--- spring_MVC_2/login/src/main/java/hello/login/web/HomeController.java
+++ spring_MVC_2/login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -53,7 +54,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);
@@ -63,7 +64,19 @@ public class HomeController {
 
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
 
-        if(loginMember != null)
+        if(loginMember == null)
+            return "home";
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember , Model model){
+
+        log.info("로그인정보:{}",loginMember);
+
+        if(loginMember == null)
             return "home";
 
         model.addAttribute("member", loginMember);
