058555250099a01ff265b012eb11bf0c9c525e17
jonghyeon95 <whdgus5289@naver.com>
2021-09-16 02:48:44 +0900

    Order Service 개발

--- springboot_JPA_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ springboot_JPA_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -37,6 +40,9 @@ public class OrderItem {
         return orderItem;
     }
 
+
+//    protected OrderItem() {   //롬복으로 대체
+//    }
 
     //==비즈니스 로직==//
     public void cancel() {
