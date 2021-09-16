78c7734d525fbb95ff99a88ba7b6145cd2a684e6
jonghyeon95 <whdgus5289@naver.com>
2021-09-17 01:13:50 +0900

    Member 회원가입 완료

---
+++ springboot_JPA_1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm_";
+    }
+
+    @PostMapping("/members/new")
+    public String addMember(@Valid @ModelAttribute MemberForm memberForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info(bindingResult.toString());
+            return "members/createMemberForm_";
+        }
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode()));
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
