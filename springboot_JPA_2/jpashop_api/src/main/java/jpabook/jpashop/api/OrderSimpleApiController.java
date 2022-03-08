b2c51937a6d2f5205e5bbaaddcf8fbe8b0f6a5f7
jonghyeon95 <whdgus5289@naver.com>
2022-03-08 23:12:47 +0900

    -Order 조회 V2 (xToOne 일때)  Dto를 이용한 조회 (but, 지연로딩으로 인해 N+1문제발생)

--- springboot_JPA_2/jpashop_api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ springboot_JPA_2/jpashop_api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.Domain.Address;
+import jpabook.jpashop.Domain.Enum.OrderStatus;
 import jpabook.jpashop.Domain.Order;
 import jpabook.jpashop.Dto.OrderSearch;
 import jpabook.jpashop.Repository.OrderRepository;
 import jpabook.jpashop.Service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne 에서의 성능 최적화방법
@@ -23,6 +29,7 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    //================주문조회 v1=================//
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllBySearch(new OrderSearch());
@@ -32,6 +39,39 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    //================주문조회 v2=================//
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        List<Order> orders = orderRepository.findAllBySearch(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //Lazy 초기화
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
 
 
 }
