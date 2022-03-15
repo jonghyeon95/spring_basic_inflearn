6be169cc7bfcfaf1169ff3efdb3cc3c22dd4dffc
jonghyeon95 <whdgus5289@naver.com>
2022-03-15 16:51:49 +0900

    -queryDsl 기본

--- SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import study.querydsl.Entity.Hello;
-import study.querydsl.Entity.Qhello;
-
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,18 +20,6 @@ class QuerydslApplicationTests {
 	@Test
 	void contextLoads() {
 
-		Hello hello = new Hello();
-		em.persist(hello);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-//		Qhello qhello = new Qhello("h");
-		Qhello qhello = Qhello.hello;
-
-		Hello result = query.selectFrom(qhello)
-				.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
 
 
 
