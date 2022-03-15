8fdc28348398cd1ab4a03868df95a790925796c8
jonghyeon95 <whdgus5289@naver.com>
2022-03-15 17:25:15 +0900

    -정렬

--- SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -94,28 +94,50 @@ public class QuerydslBasicTest {
 
     @Test
     public void resultFetch() throws Exception {
-//        List<Member> fetch = queryFactory
-//                .selectFrom(member)
-//                .fetch();
-//
-//        Member fetchOne = queryFactory
-//                .selectFrom(QMember.member)
-//                .fetchOne();
-//
-//        Member fetchFirst = queryFactory
-//                .selectFrom(QMember.member)
-//                .fetchFirst();
-//
-//        QueryResults<Member> fetchResults = queryFactory    //카운트까지 가져옴
-//                .selectFrom(member)
-//                .fetchResults();
-//
-//        fetchResults.getTotal();
-//        List<Member> content = fetchResults.getResults();
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(QMember.member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(QMember.member)
+                .fetchFirst();
+
+        QueryResults<Member> fetchResults = queryFactory    //카운트까지 가져옴
+                .selectFrom(member)
+                .fetchResults();
+
+        fetchResults.getTotal();
+        List<Member> content = fetchResults.getResults();
 
         queryFactory
                 .selectFrom(member)
                 .fetchCount();
+    }
+
+    @Test
+    public void sort() throws Exception {
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
 
     }
 
