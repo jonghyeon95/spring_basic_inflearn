0574e554c1650f1edef96aaa0f607a3c73bd661b
jonghyeon95 <whdgus5289@naver.com>
2022-03-16 18:38:07 +0900

    join

--- SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -211,7 +211,94 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    @Test
+    public void join() throws Exception {
+        //TeamA소속된 모든회원
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .contains("member1", "member2");
+    }
+
+    @Test
+    public void theta_join() throws Exception {
+        //억지 조인, 팀이름과 사람이름이 같은 회원 조회
+
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .contains("teamA", "teamB");
+    }
+
+    @Test
+    public void join_on_filtering() throws Exception {
+        //회원과 팀을 조인, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+
+        //JPQL
+//        List<Object[]> resultList = em.createQuery("select m, t from Member m left join m.team t on t.name = 'teamA'").getResultList();
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple.get(member).getUsername());
+        }
+
+    }
+
+    @Test
+    public void join_on_no_relation() throws Exception {
+        //억지 조인, 팀이름과 사람이름이 같은 회원 조회
+        //left 조인
+
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team) //막조인 같은경우는 team 하나만 들어감
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void fetchJoin() throws Exception {
+        em.flush();
+        em.clear();
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+    }
+
     
+
 }
 
 
