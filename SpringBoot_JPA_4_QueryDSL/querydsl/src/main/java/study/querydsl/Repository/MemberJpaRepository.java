d3b4879f9cb7de5b2107678a5d0d35ec326a22d3
jonghyeon95 <whdgus5289@naver.com>
2022-03-17 21:15:48 +0900

    동적쿼리 builder 사용예제

--- SpringBoot_JPA_4_QueryDSL/querydsl/src/main/java/study/querydsl/Repository/MemberJpaRepository.java
+++ SpringBoot_JPA_4_QueryDSL/querydsl/src/main/java/study/querydsl/Repository/MemberJpaRepository.java
@@ -1,16 +1,24 @@
 package study.querydsl.Repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.Dto.MemberSearchCondition;
+import study.querydsl.Dto.MemberTeamDto;
+import study.querydsl.Dto.QMemberDto;
+import study.querydsl.Dto.QMemberTeamDto;
 import study.querydsl.Entity.Member;
 import study.querydsl.Entity.QMember;
+import study.querydsl.Entity.QTeam;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 import static study.querydsl.Entity.QMember.*;
+import static study.querydsl.Entity.QTeam.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -49,4 +57,29 @@ public class MemberJpaRepository {
                 .where(member.username.eq(username))
                 .fetch();
     }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (StringUtils.hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(member.id, member.username, member.age, team.id, team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
 }
--- SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/Repository/MemberJpaRepositoryTest.java
+++ SpringBoot_JPA_4_QueryDSL/querydsl/src/test/java/study/querydsl/Repository/MemberJpaRepositoryTest.java
@@ -1,11 +1,16 @@
 package study.querydsl.Repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.Dto.MemberSearchCondition;
+import study.querydsl.Dto.MemberTeamDto;
 import study.querydsl.Entity.Member;
+import study.querydsl.Entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -22,6 +27,23 @@ class MemberJpaRepositoryTest {
     EntityManager em;
     @Autowired
     MemberJpaRepository memberJpaRepository;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
 
     @Test
@@ -57,5 +79,17 @@ class MemberJpaRepositoryTest {
         assertThat(result2).containsExactly(member);
     }
 
+    @Test
+    public void searchTestByBuilder() throws Exception {
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+//        condition.setUsername();
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("username").contains("member4");
+    }
 
 }
